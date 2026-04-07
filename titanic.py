4f7d15c036a8b78fa5b00ae67412a8c87b87dfbf
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-04-07 09:05:23 +0000

    import streamlined

--- titanic.py
+++ titanic.py
@@ -4,8 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 import duckdb
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 
-# os.chdir('/home/coder/work/ensae-reproductibilite-application')
 titanic = pd.read_csv("data.csv")
 
 
@@ -24,16 +26,12 @@ else:
     print(f"Problème dans la colonne Name: {bad} ne se décomposent pas en 2 parties.")
 
 
-n_trees = 20
+N_TREES = 20
 max_depth = None
 max_features = "sqrt"
 
 
 ## Encoder les données imputées ou transformées.
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-
 numeric_features = ["Age", "Fare"]
 categorical_features = ["Embarked", "Sex"]
 
@@ -75,7 +73,8 @@ pipe = Pipeline(
 y = titanic["Survived"]
 X = titanic.drop("Survived", axis="columns")
 
-# On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée une partie pour apprendre une partie pour regarder le score.
+# On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée
+# une partie pour apprendre une partie pour regarder le score.
 # Prenons arbitrairement 10% du dataset en test et 90% pour l'apprentissage.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
@@ -125,18 +124,12 @@ print(message)
 
 
 # Random Forest
-
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier
-
-
 # Ici demandons d'avoir 20 arbres
 pipe.fit(X_train, y_train)
 
 
-# calculons le score sur le dataset d'apprentissage et sur le dataset de test (10% du dataset d'apprentissage mis de côté)
+# calculons le score sur le dataset d'apprentissage et sur le dataset de test
+# (10% du dataset d'apprentissage mis de côté)
 # le score étant le nombre de bonne prédiction
 rdmf_score = pipe.score(X_test, y_test)
 rdmf_score_tr = pipe.score(X_train, y_train)
