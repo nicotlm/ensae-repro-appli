8924787b1d4d1415213df409a57d599ebf380fe1
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-04-07 09:12:04 +0000

    update appli 2

--- titanic.py
+++ titanic.py
@@ -1,19 +1,37 @@
-import pandas as pd
-import duckdb
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+"""
+Prediction de la survie d'un individu sur le Titanic
+"""
+
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
+
+import pandas as pd
+import duckdb
+
+
+N_TREES = 20
+MAX_DEPTH = None
+MAX_FEATURES = "sqrt"
+NUMERIC_FEATURES = ["Age", "Fare"]
+CATEGORICAL_FEATURES = ["Embarked", "Sex"]
+
+JETON_API = "$trotskitueleski1917"
+
+con = duckdb.connect(database=":memory:")
 
 titanic = pd.read_csv("data.csv")
 
 
-con = duckdb.connect(database=":memory:")
+# QUALITY DIAGNOSTICS  ---------------------------------------
 
-# Check la structure de Name "Nom, Prénom"
+# TEST NAME FORMATTING ==============================
+
 bad = con.sql("""
     SELECT COUNT(*) AS n_bad
     FROM titanic
@@ -26,79 +44,8 @@ else:
     print(f"Problème dans la colonne Name: {bad} ne se décomposent pas en 2 parties.")
 
 
-N_TREES = 20
-MAX_DEPTH = None
-MAX_FEATURES = "sqrt"
+# CHECK MISSING VALUES ==============================
 
-
-## Encoder les données imputées ou transformées.
-numeric_features = ["Age", "Fare"]
-categorical_features = ["Embarked", "Sex"]
-
-numeric_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="median")),
-        ("scaler", MinMaxScaler()),
-    ]
-)
-
-categorical_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="most_frequent")),
-        ("onehot", OneHotEncoder()),
-    ]
-)
-
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("Preprocessing numerical", numeric_transformer, numeric_features),
-        (
-            "Preprocessing categorical",
-            categorical_transformer,
-            categorical_features,
-        ),
-    ]
-)
-
-pipe = Pipeline(
-    [
-        ("preprocessor", preprocessor),
-        ("classifier", RandomForestClassifier(n_estimators=20)),
-    ]
-)
-
-
-# splitting samples
-y = titanic["Survived"]
-X = titanic.drop("Survived", axis="columns")
-
-# On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée
-# une partie pour apprendre une partie pour regarder le score.
-# Prenons arbitrairement 10% du dataset en test et 90% pour l'apprentissage.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-
-# check que pas de problème de data leakage
-if set(X_train["Embarked"].dropna().unique()) - set(
-    X_test["Embarked"].dropna().unique()
-):
-    message = "Problème de data leakage pour la variable Embarked"
-else:
-    message = "Pas de problème de data leakage pour la variable Embarked"
-
-print(message)
-
-if set(X_train["Sex"].dropna().unique()) - set(X_test["Sex"].dropna().unique()):
-    message = "Problème de data leakage pour la variable Sex"
-else:
-    message = "Pas de problème de data leakage pour la variable Embarked"
-
-print(message)
-
-
-TOKEN_API = "$trotskitueleski1917"
-
-# Vérifie les valeurs manquantes
 # TODO: généraliser à toutes les variables
 n_missing = con.sql("""
     SELECT COUNT(*) AS n_missing
@@ -123,16 +70,78 @@ message = message_ok if n_missing == 0 else message_warn
 print(message)
 
 
-# Random Forest
-# Ici demandons d'avoir 20 arbres
+# MODEL -----------------------------------------
+
+y = titanic["Survived"]
+X = titanic.drop("Survived", axis="columns")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+
+numeric_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", MinMaxScaler()),
+    ]
+)
+
+categorical_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder()),
+    ]
+)
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("Preprocessing numerical", numeric_transformer, NUMERIC_FEATURES),
+        (
+            "Preprocessing categorical",
+            categorical_transformer,
+            CATEGORICAL_FEATURES,
+        ),
+    ]
+)
+
+pipe = Pipeline(
+    [
+        ("preprocessor", preprocessor),
+        (
+            "classifier",
+            RandomForestClassifier(
+                n_estimators=N_TREES, max_depth=MAX_DEPTH, max_features=MAX_FEATURES
+            ),
+        ),
+    ]
+)
+
+
+# PIPELINE VALIDATION -----------------------------------
+
+if set(X_train["Embarked"].dropna().unique()) - set(
+    X_test["Embarked"].dropna().unique()
+):
+    message = "Problème de data leakage pour la variable Embarked"
+else:
+    message = "Pas de problème de data leakage pour la variable Embarked"
+
+print(message)
+
+if set(X_train["Sex"].dropna().unique()) - set(X_test["Sex"].dropna().unique()):
+    message = "Problème de data leakage pour la variable Sex"
+else:
+    message = "Pas de problème de data leakage pour la variable Embarked"
+
+print(message)
+
+
+# TRAINING AND EVALUATION --------------------------------------------
+
 pipe.fit(X_train, y_train)
-
-
-# calculons le score sur le dataset d'apprentissage et sur le dataset de test
-# (10% du dataset d'apprentissage mis de côté)
-# le score étant le nombre de bonne prédiction
 rdmf_score = pipe.score(X_test, y_test)
 rdmf_score_tr = pipe.score(X_train, y_train)
+
 print(f"{rdmf_score:.1%} de bonnes réponses sur les données de test pour validation")
 
 print(20 * "-")
